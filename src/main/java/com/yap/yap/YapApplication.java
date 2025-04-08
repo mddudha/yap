76d34536776c4ffc32c25f78657741f56f94ac23
Mirva Dudhagara <middudha@iu.edu>
2025-04-08 16:26:11 -0400

    finished Yap

---
+++ src/main/java/com/yap/yap/YapApplication.java
@@ -0,0 +1,13 @@
+package com.yap.yap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yap/yap/YapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yap.yap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
